121eedc464b1423343b1382c570c5c4ed9f48d64
ваше имя <dimarik-tixonov@yandex.ru>
2022-07-17 22:34:39 +0300

    add: стилизует footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,29 @@
 import { FooterProps } from "./Footer.Props";
+import cn from "classnames";
+import { format } from "date-fns";
 import styles from './Footer.module.css';
 
-export const Footer = ({...props}: FooterProps):JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps):JSX.Element => {
     return (
-        <div
+        <footer className={cn(className, styles.footer)}
             {...props}
         >
-            Footer
-        </div>
+            <div>
+                OwlTop © 2020 - {format(new Date(), 'y')} Все права защищены
+            </div>
+            <a
+                href="#"
+                target="_blank" 
+                className={styles.link}
+            >
+                Пользовательское соглашение
+            </a>
+            <a 
+                href="#"
+                className={styles.link}
+            >
+                Политика конфиденциальности
+            </a>
+        </footer>
     );
 };
